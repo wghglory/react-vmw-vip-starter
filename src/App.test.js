39ab1a0d87fb0a55e3e32cd9186667ee8a470294
Guanghui Wang <guanghui-wang@foxmail.com>
2021-03-13 22:22:39 +0800

    chore: move devDependencies; test setup

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  debugger;
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
